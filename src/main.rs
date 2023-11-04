e3f166626bb214ceb37e221c5a201731e6c04cc6
k-kbot <polykkpodh1243@gmail.com>
2023-11-04 14:32:07 +0900

    create task

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use crate::models::task::Task;
+
+mod models;
 
 async fn greet() -> impl Responder {
     "Hello!"
@@ -10,12 +13,18 @@ async fn get_tasks() -> impl Responder {
     ])
 }
 
+async fn create_task(task: web::Json<Task>) -> impl Responder {
+    println!("Creating new task: {:?}", task);
+    HttpResponse::Created().json(task.into_inner())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/tasks", web::get().to(get_tasks))
+            .route("/tasks", web::post().to(create_task))
     })
         .bind("127.0.0.1:8080")?
         .run()

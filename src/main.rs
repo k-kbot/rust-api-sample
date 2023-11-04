dc672be357afd6850b7b3f1b550c7cdbb87de73e
k-kbot <polykkpodh1243@gmail.com>
2023-11-04 15:00:17 +0900

    test root

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use crate::models::task::Task;
 
+
 mod models;
 
 async fn greet() -> impl Responder {
-    "Hello!"
+    HttpResponse::Ok().body("Hello!")
 }
 
 async fn get_tasks() -> impl Responder {
@@ -29,4 +30,22 @@ async fn main() -> std::io::Result<()> {
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use actix_web::{test, web::Bytes};
+
+    #[actix_rt::test]
+    async fn test_greet() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(greet))).await;
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+        let body = test::read_body(resp).await;
+        assert_eq!(body, Bytes::from_static(b"Hello!"));
+    }
 }

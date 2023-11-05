043833ac85cdbb36f9716a0cb8297a80b7b862db
k-kbot <polykkpodh1243@gmail.com>
2023-11-05 13:53:53 +0900

    add status to tasks

--- src/models/task.rs
+++ src/models/task.rs
@@ -5,10 +5,26 @@ pub struct Task {
     pub id: u32,
     pub title: String,
     pub description: String,
+    pub status: TaskStatus,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NewTask {
     pub title: String,
     pub description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum TaskStatus {
+    Todo = 0,
+    Done = 1,
+}
+
+impl TaskStatus {
+    pub fn value(&self) -> i32 {
+        match *self {
+            TaskStatus::Todo => 0,
+            TaskStatus::Done => 1,
+        }
+    }
 }
